d25e209953ff2b2e3c0163e164119971a5eecdb9
Salladoretz <87745737+Salladoretz@users.noreply.github.com>
2022-10-10 11:30:58 +1000

    Update script.php

--- script.php
+++ script.php
@@ -3,6 +3,9 @@ require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
 $APPLICATION->SetTitle("1");
 ?>
 <?
+
+if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+
 CModule::IncludeModule("iblock");
 
 $ibId = 8; //Id инфоблока
